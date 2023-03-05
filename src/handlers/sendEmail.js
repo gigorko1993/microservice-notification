dbd2fab2c633f9ab43492022f79d57bd1d65a290
Igor Mokhnatskyy <55652900+gigorko1993@users.noreply.github.com>
2023-03-05 14:36:50 +0200

    set up notification service

--- src/handlers/sendEmail.js
+++ src/handlers/sendEmail.js
@@ -3,25 +3,31 @@ const AWS = require('aws-sdk');
 const ses = new AWS.SES({ region: 'eu-west-1' });
 
 const handler = async (event, context) => {
+  console.log("event: ", event);
+  const record = event.Records[0];
+  console.log("record: ", record);
+
+  const { subject, body, recipient } = JSON.parse(record.body);
+
   const params = {
     Source: 'gigorko35@gmail.com',
     Destination: {
-      ToAddresses: ['gigorko35@gmail.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from auction service'
+          Data: body
         }
       },
       Subject: {
-        Data: 'Test email from auction'
+        Data: subject
       }
     }
   }
   try {
     const result = await ses.sendEmail(params).promise();
-    console.oog("result: ", result);
+    console.log("result: ", result);
     return result
   } catch (err) {
     console.error(err)
